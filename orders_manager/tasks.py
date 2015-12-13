3a3d6c31f889e83930151dd7c95a776814cb2c7d
Ra3bOjN1K <Ra3bOjN1K@mail.ru>
2015-12-13 16:15:28 +0300

    Added test tasks to orders_manager module

---
+++ orders_manager/tasks.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from celery import Celery
+from django.conf import settings
+
+BROKER = 'amqp://{user}:{pass}@{host}:5672//'.format(
+    settings.RABBITMQ_USER, settings.RABBITMQ_PASSWORD, settings.RABBITMQ_HOST)
+
+app = Celery('tasks', broker=BROKER)
+
+
+@app.task
+def add(x, y):
+    return x + y
