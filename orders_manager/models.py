d4b82319a947f2d1b1d33d43fdeb309f6917f8c0
Ra3bOjN1K <Ra3bOjN1K@mail.ru>
2016-03-02 19:40:40 +0300

    sms delivery -> final version(set default value for sender in the model).

--- orders_manager/models.py
+++ orders_manager/models.py
@@ -371,7 +371,7 @@ class AnimatorDebt(models.Model):
 class SmsDeliveryCredentials(models.Model):
     login = models.CharField(max_length=60)
     password = models.CharField(max_length=120)
-    sender = models.CharField(max_length=60)
+    sender = models.CharField(max_length=60, default='')
 
 
 class SmsDeliveryEvent(models.Model):
